3a9cfbe22fbd15c893d9746f773461f8bd53c8e0
Slava Oleinik <ol_slava@mail.ru>
2020-07-29 23:43:44 +0300

    delete

--- maven-automation-tasks/AircompanyForJavaCollections/AirplaneType.java
+++
@@ -1,6 +0,0 @@
-package com.javaweb.model.entity;
-
-public enum AirplaneType {
-    PASSENGER_NARROW, PASSENGER_WIDE, AGRICULTURAL_MONOPLANE,
-    AGRICULTURAL_BIPLANE, FREIGHT_LIGHT, FREIGHT_HEAVY
-}
